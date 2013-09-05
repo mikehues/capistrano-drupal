fe19f5e749c37e5d720e3767ce035805446bd0da
Michael Hughes <mikebhughes@gmail.com>
2013-09-05 10:27:31 -0400

    allow the option to symlink additional assets. for example, .htaccess and robots.txt

--- lib/capistrano-drupal.rb
+++ lib/capistrano-drupal.rb
@@ -20,6 +20,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   set(:deploy_to) { "/var/www/#{application}" }
   set :shared_children, ['files', 'private']
+  set :additional_shared_assets, []
 
   after "deploy:update_code", "drupal:symlink_shared", "drush:site_offline", "drush:updatedb", "drush:cache_clear", "drush:site_online"
 
@@ -48,10 +49,15 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   namespace :drupal do
     desc "Symlink settings and files to shared directory. This allows the settings.php and \
-      and sites/default/files directory to be correctly linked to the shared directory on a new deployment."
+      and sites/default/files directory to be correctly linked to the shared directory on a new deployment. \
+      Also, symlink and assets specified with :additional_shared_assets."
     task :symlink_shared do
       ["files", "private", "settings.php"].each do |asset|
         run "rm -rf #{app_path}/#{asset} && ln -nfs #{shared_path}/#{asset} #{app_path}/sites/default/#{asset}"
+      end
+
+      additional_shared_assets.each do |asset|
+        run "rm -rf #{app_path}/#{asset} && ln -nfs #{shared_path}/#{asset} #{app_path}/#{asset}"
       end
     end
   end
